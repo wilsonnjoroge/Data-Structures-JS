a15ae5cb909c7b5cc1d07c3b0fba3b62fe380160
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-04-24 20:57:44 +0300

    Added an optimized practice code for queues

--- queue_optimized.js
+++ queue_optimized.js
@@ -34,4 +34,37 @@ class Queue {
     return this.queue[this.front]
   }
 
+  size() {
+    return this.rear - this.front
+  }
+
+  print() {
+    console.log(this.queue)
+  }
+
 }
+
+// Create a queue
+const queue = new Queue()
+
+// To add a new queue
+queue.enqueue(11);
+queue.enqueue(12);
+queue.enqueue(13);
+queue.enqueue(14);
+queue.enqueue(15);
+
+console.log(queue)
+
+// To get the size of the queue
+console.log(queue.size())
+
+// To remove item from queue (The one entered first)
+queue.dequeue();
+console.log(queue)
+
+// To get the first item in the queue
+console.log(queue.peek())
+
+// To check if the queue is empty
+console.log(queue.isEmpty())
