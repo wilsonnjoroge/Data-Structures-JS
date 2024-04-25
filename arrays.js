90a547c511e82cd626c72866112a3b7690363871
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-04-25 21:29:37 +0300

    Added a problem and its solutions

--- arrays.js
+++ arrays.js
@@ -43,14 +43,14 @@ console.log(evenNumbers) // will output [0,2,4]
 
 
 // USE OF REDUCE() METHOD
-// 1. To find maximum in an array using if
+// 1. To find maximum in an array using tenary operator
  const max = arr.reduce((max, current) => {
   return current > max ? current : max
  }, arr[0])
 
  console.log(max) // will output 5
 
- // 2. To find maximum in an array using tenary operator
+ // 2. To find maximum in an array using if
  const maxx = arr.reduce((maxx, currrent) => {
   
     if(currrent > maxx) {
@@ -158,3 +158,29 @@ for (let i = 0; i < newArray8.length; i++) {
 }
 
 console.log(newArray8); 
+
+
+/*
+When duplicates are removed from an array, and only even numbers are left, 
+array A[3,5,3,6,8,3,8,4] will result to   [ 6, 8, 4 ]. and B [56,45,33,98,67,22] 
+will result to [ 56, 98, 22 ].  Given the  Array C = [2, 4, 8, 8, 7, 5, 5, 3, 2], 
+write an effective algorithm (using array methods) to ensure that the final array 
+is a result containing even numbers and have no duplicates and log the result
+*/
+
+function solution(A) {
+
+  const newArray = []
+  
+  for(let i = 0; i < A.length; i++) {
+    if(!newArray.includes(A[i])) {
+      newArray.push(A[i]);
+    }
+  }
+  const evenArray = newArray.filter(number => {  return number%2 === 0})
+
+  return evenArray
+
+}
+
+console.log(solution([3,5,3,6,8,3,8,4]))
